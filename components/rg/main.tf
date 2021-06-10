92c12fc65c17e73c66782dcc602b46c2bbd73a53
Cary Corse <cary.corse@evoco.co.uk>
2021-06-10 21:34:32 +0100

    addin sbox tfvars and adding sa creation

--- components/rg/main.tf
+++ components/rg/main.tf
@@ -2,3 +2,14 @@ resource "azurerm_resource_group" "rg" {
   name     = "pih-sharedinfra-${var.env}-rg"
   location = "UK South"
 }
+
+module "storage_account" {
+  source                   = "git@github.com:hmcts/cnp-module-storage-account.git?ref=master"
+  env                      = ${var.env}
+  storage_account_name     = ${var.storage_account_name}
+  location                 = var.azurerm_location
+  access_tier              = var.access_tier
+  team_contact             = "#vh-devops"
+  team_name                = "Video Hearings"
+
+}
