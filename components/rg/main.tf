3ca8af00b1be22347ecbd3ed29fb03f213c7c095
Cary Corse <cary.corse@evoco.co.uk>
2021-06-11 05:53:36 +0100

    storage accound and rg names

--- components/rg/main.tf
+++ components/rg/main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "pih-sharedinfra-${var.env}-rg"
+  name     = "pip-sharedinfra-${var.env}-rg"
   location = "UK South"
 }
 
 module "storage_account" {
   source                   = "git@github.com:hmcts/cnp-module-storage-account.git?ref=master"
   env                      = "${var.env}"
-  storage_account_name     = "${var.storage_account_name}"
+  storage_account_name     = "pip-sharedinfra-${var.env}-sa"
 
-  
+
 }
