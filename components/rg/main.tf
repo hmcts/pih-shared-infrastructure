a4701f2e52eacb2ed968e5bf207ba577746683a8
Cary Corse <cary.corse@evoco.co.uk>
2021-06-11 10:33:46 +0100

    shortened sa and rg names

--- components/rg/main.tf
+++ components/rg/main.tf
@@ -1,13 +1,13 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "pip-sharedinfra-${var.env}-rg"
+  name     = "pip-shared-${var.env}-rg"
   location = "UK South"
 }
 
 module "storage_account" {
   source               = "github.com/hmcts/cnp-module-storage-account.git?ref=Fix_Vars"
   env                  = var.env
-  storage_account_name = "pip-sharedinfra-${var.env}-sa"
+  storage_account_name = "pip-shared-${var.env}-sa"
   account_kind         = "Storage"
-  resource_group_name  = "pip-sharedinfra-${var.env}-rg"
+  resource_group_name  = "pip-shared-${var.env}-rg"
 
 }
