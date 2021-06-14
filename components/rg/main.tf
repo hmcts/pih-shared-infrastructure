3c463eceb84623504670fbf51e52efa29984adab
Cary Corse <cary.corse@evoco.co.uk>
2021-06-14 10:39:22 +0100

    updating storage type

--- components/rg/main.tf
+++ components/rg/main.tf
@@ -7,7 +7,7 @@ module "storage_account" {
   source               = "github.com/hmcts/cnp-module-storage-account.git?ref=Fix_Vars"
   env                  = var.env
   storage_account_name = "pipshared${var.env}sa"
-  account_kind         = "Storage"
+  account_kind         = "StorageV2"
   resource_group_name  = "pipshared${var.env}rg"
 
 }
