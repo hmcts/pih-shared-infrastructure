106cb8186ab94412db88594020d40549f2e78002
Cary Corse <cary.corse@evoco.co.uk>
2021-06-15 16:14:44 +0100

    changing type

--- components/kv/main.tf
+++ components/kv/main.tf
@@ -10,7 +10,7 @@ module "claim-store-vault" {
   env                 = var.env
   object_id           = var.jenkins_AAD_objectId
   resource_group_name = azurerm_resource_group.rg.name
-  product_group_name  = "Your AAD group" # e.g. MI Data Platform, or dcd_cmc
+  product_group_name  = "DTS Publishing and Information" # e.g. MI Data Platform, or dcd_cmc
   common_tags         = var.common_tags
 }
 
