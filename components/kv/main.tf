fbf43cd782443952692c883e0dff6542811965f7
Cary Corse <cary.corse@evoco.co.uk>
2021-06-15 19:40:09 +0100

    adding keyvault

--- components/kv/main.tf
+++ components/kv/main.tf
@@ -5,10 +5,10 @@ resource "azurerm_resource_group" "rg" {
 
 module "claim-store-vault" {
   source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                = "rhubarb-fe-${var.env}" // Max 24 characters
-  product             = var.product
-  env                 = var.env
-  object_id           = var.jenkins_AAD_objectId
+  name                = "pip-shared-kv-${var.env}" // Max 24 characters
+  product             = pip
+  env                 = ${var.env}
+  object_id           = "DTS Key Vault Administrators"
   resource_group_name = azurerm_resource_group.rg.name
   product_group_name  = "DTS Publishing and Information" # e.g. MI Data Platform, or dcd_cmc
   common_tags         = var.common_tags
