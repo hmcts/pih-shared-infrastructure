cfb347770da82a667a7ed06185568a491825cae6
Cary Corse <cary.corse@evoco.co.uk>
2021-06-15 20:06:11 +0100

    fmt

--- components/kv/main.tf
+++ components/kv/main.tf
@@ -4,13 +4,13 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "claim-store-vault" {
-  source              = "github.com/hmcts/cnp-module-key-vault"
-  name                = "pip-shared-kv-${var.env}" // Max 24 characters
-  product             = "pip"
-  env                 = var.env
-  object_id           = "DTS Key Vault Administrators"
-  resource_group_name = "pipshared${var.env}rg"
-  product_group_name  = "DTS Publishing and Information" # e.g. MI Data Platform, or dcd_cmc
+  source                  = "github.com/hmcts/cnp-module-key-vault"
+  name                    = "pip-shared-kv-${var.env}" // Max 24 characters
+  product                 = "pip"
+  env                     = var.env
+  object_id               = "DTS Key Vault Administrators"
+  resource_group_name     = "pipshared${var.env}rg"
+  product_group_name      = "DTS Publishing and Information" # e.g. MI Data Platform, or dcd_cmc
   create_managed_identity = true
 
 
