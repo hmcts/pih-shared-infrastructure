447871cf49ffcedb399b0f998156e88e1dcf7e5a
Cary Corse <cary.corse@evoco.co.uk>
2021-06-15 19:50:20 +0100

    adding env

--- components/vnet/main.tf
+++ components/vnet/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "rg" {
 module "vnet" {
   source                = "git@github.com:hmcts/cnp-module-vnet?ref=fix-address-prefix"
   name                  = "pip-sharedinfra-vnet-${var.env}"
-  location              = var.location
+  location              = "UK South"
   address_space         = "10.101.1.0/26"
   source_range          = "10.101.1.0/26"
   env                   = "${var.env}"
