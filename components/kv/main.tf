9d9f6cefa4a25a4a47f5f80d25f400437b5f9225
Cary Corse <cary.corse@evoco.co.uk>
2021-06-15 19:47:41 +0100

    formatting

--- components/kv/main.tf
+++ components/kv/main.tf
@@ -7,7 +7,7 @@ module "claim-store-vault" {
   source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name                = "pip-shared-kv-${var.env}" // Max 24 characters
   product             = pip
-  env                 = var.env
+  env                 = "${var.env}"
   object_id           = "DTS Key Vault Administrators"
   resource_group_name = azurerm_resource_group.rg.name
   product_group_name  = "DTS Publishing and Information" # e.g. MI Data Platform, or dcd_cmc
