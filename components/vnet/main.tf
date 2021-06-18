2aed8a382cd1bada54fd72c460d471f70a2846a2
lowflying <chriscm.neu@gmail.com>
2021-06-18 12:14:22 +0100

    fixed vnet common tags

--- components/vnet/main.tf
+++ components/vnet/main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "pip-sharedinfra-"
+  name     = "pip-sharedinfra"
   location = "UK South"
 }
 
 module "vnet" {
   source                = "github.com/hmcts/cnp-module-vnet?ref=fix-address-prefix"
-  name                  = "pip-sharedinfra-"
+  name                  = "pip-sharedinfra"
   location              = "UK South"
   address_space         = "10.101.1.0/26"
   source_range          = "10.101.1.0/26"
