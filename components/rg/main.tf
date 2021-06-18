5ad2d8188cedc536f08a42876546edcf1f70fce6
lowflying <chriscm.neu@gmail.com>
2021-06-18 15:59:14 +0100

    uncommented sa creation

--- components/rg/main.tf
+++ components/rg/main.tf
@@ -15,12 +15,12 @@ resource "azurerm_resource_group" "rg2" {
 
 
 
-# module "storage_account" {
-#   source                 = "github.com/hmcts/cnp-module-storage-account.git?ref=Fix_Vars"
-#   env                    = var.env
-#   storage_account_name   = "pipshared${var.env}sa"
-#   account_kind           = "BlockBlobStorage"
-#   resource_group_name    = "pipshared${var.env}rg"
-#   enable_data_protection = "true"
+module "storage_account" {
+  source                 = "github.com/hmcts/cnp-module-storage-account.git?ref=Fix_Vars"
+  env                    = var.env
+  storage_account_name   = "pipshared${var.env}sa"
+  account_kind           = "BlockBlobStorage"
+  resource_group_name    = "pipshared${var.env}rg"
+  enable_data_protection = "true"
 
-# }
+}
