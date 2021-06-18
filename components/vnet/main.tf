fc49fc452097a5ebb93f4b92da1dc557508c2b24
lowflying <chriscm.neu@gmail.com>
2021-06-18 14:04:56 +0100

    changed source addresses

--- components/vnet/main.tf
+++ components/vnet/main.tf
@@ -7,8 +7,8 @@ module "vnet" {
   source                = "github.com/hmcts/cnp-module-vnet?ref=fix-address-prefix"
   name                  = "pip-sharedinfra"
   location              = "UK South"
-  address_space         = "10.101.1.0/12"
-  source_range          = "10.101.1.0/26"
+  address_space         = "10.101.1.0/26"
+  source_range          = "10.101.1.0/12"
   env                   = var.env
   lb_private_ip_address = cidrhost(cidrsubnet(var.address_space, 4, 2), -2)
   common_tags           = tomap({
