86af36926cbfd2dbef67f9bfed598ce90cd58daa
Christopher Pateman <christopher.pateman@version1.com>
2021-08-27 16:02:58 +0100

    PUB-674 remove run time param and update dependson format

--- components/rg/main.tf
+++ components/rg/main.tf
@@ -1,17 +1,17 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "pipshared${var.env}rg"
-  location = "UK South"
+locals {
+  sharedServicePrefix = "pipshared${var.env}"
+  sharedInfraPrefix = "pip-sharedservices-${var.env}-"
 }
 
-resource "azurerm_resource_group" "rg1" {
-  name     = "pip-sharedservices-${var.env}-rg"
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.sharedServicePrefix}rg"
   location = "UK South"
 }
 
 module "storage_account" {
   source                 = "github.com/hmcts/cnp-module-storage-account.git?ref=Fix_Vars"
   env                    = var.env
-  storage_account_name   = "pipshared${var.env}sa"
+  storage_account_name   = "${local.sharedServicePrefix}sa"
   account_kind           = "BlockBlobStorage"
   resource_group_name    = azurerm_resource_group.rg1.name
   enable_data_protection = "true"
@@ -19,6 +19,11 @@ module "storage_account" {
 
 resource "azurerm_storage_container" "piptfstateterraform" {
   name                  = "piptfstateterraform"
-  storage_account_name  = "pipshared${var.env}sa"
+  storage_account_name  = "${local.sharedServicePrefix}sa"
   container_access_type = "private"
+}
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "${local.sharedInfraPrefix}-rg"
+  location = "UK South"
 }
